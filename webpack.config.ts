921475535a90d4e3703f45952332e3ff5b951b97
cola119 <k.cola119@ezweb.ne.jp>
2021-05-01 14:58:45 +0900

    fix build failure

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,35 +1,35 @@
-import { ConfigurationFactory } from "webpack";
+
 import path from "path";
 import CopyWebpackPlugin from "copy-webpack-plugin";
+import { Configuration } from "webpack";
 
-const config: ConfigurationFactory = () => {
-  return {
-    entry: {
-      background: path.join(__dirname, "src", "background.ts"),
-      popup: path.join(__dirname, "src", "popup.ts"),
-    },
-    output: {
-      path: path.join(__dirname, "dist"),
-      filename: "[name].js",
-    },
-    module: {
-      rules: [
-        {
-          test: /.ts$/,
-          use: "ts-loader",
-          exclude: "/node_modules/",
-        },
-      ],
-    },
-    resolve: {
-      extensions: [".ts", ".js"],
-    },
-    plugins: [
-      new CopyWebpackPlugin({
-        patterns: [{ from: "public", to: "." }],
-      }),
+const config: Configuration = {
+  entry: {
+    background: path.join(__dirname, "src", "background.ts"),
+    popup: path.join(__dirname, "src", "popup.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /.ts$/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
     ],
-  };
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public", to: "." }],
+    }),
+  ],
+
 };
 
 export default config;
