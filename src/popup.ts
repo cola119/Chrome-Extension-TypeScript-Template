e06998bc0240be281fac5a74fe89e88bc8da6f2f
cola119 <k.cola119@ezweb.ne.jp>
2021-05-01 15:18:46 +0900

    tutorial

--- src/popup.ts
+++ src/popup.ts
@@ -1,14 +1,22 @@
 let changeColor = document.getElementById("changeColor")!!;
 
-chrome.storage.sync.get("color", (data) => {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute("value", data.color);
+chrome.storage.sync.get("color", ({ color }) => {
+  changeColor.style.backgroundColor = color;
 });
-changeColor.onclick = (element: any) => {
-  let color = element.target.value;
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    chrome.tabs.executeScript(tabs[0].id!, {
-      code: 'document.body.style.backgroundColor = "' + color + '";',
-    });
+
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id! },
+    function: setPageBackgroundColor,
   });
-};
+});
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    document.body.style.backgroundColor = color;
+  });
+}
